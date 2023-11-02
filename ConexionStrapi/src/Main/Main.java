d415bdd4bdeeca43c6a97c696345552d21cfe897
Tony SC <tonysolano08@gmail.com>
2023-11-02 15:59:01 -0600

    strapi

--- ConexionStrapi/src/Main/Main.java
+++ ConexionStrapi/src/Main/Main.java
@@ -17,9 +17,8 @@ public class Main {
     public static void main(String[] args) {
 	
     	Observar("http://localhost:1337/api/casos");
-    	Agregar(1234, "Don Ramon", "Francisco", 50000);
+    	Agregar(1234, "tsolano@gmail.com");
     	Observar("http://localhost:1337/api/casos");
-    	
     }
     
     public static void Observar(String url) {
@@ -48,33 +47,39 @@ public class Main {
         }
     }
     
-    public static void Agregar(int identificacionAgricultor, String nombreAgricultor, String nombreAsesor, double deuda) {
+    public static void Agregar(int SecretCode, String Email) {
     	OkHttpClient cliente = new OkHttpClient();
 
         // URL de la colección "Casos" en Strapi
-        String url = "http://localhost:1337/api/casos"; // Asegúrate de que esta sea la URL correcta de tu colección
+        String url = "http://localhost:1337/casos/auth/local/register"; // Asegúrate de que esta sea la URL correcta de tu colección
 
-        // Crear un objeto JSON con los datos
-        String jsonDatos = "{\"IdentificacionAgricultor\":\"" + identificacionAgricultor + "\", \"NombreAgricultor\":\"" + nombreAgricultor + "\", \"NombreAsesor\":\"" + nombreAsesor + "\", \"Deuda\":" + deuda + "}";
-
+        // Datos para la nueva entrada
+        String jsonBody = "{\"secretCode\": \"eosa\", \"Email\": \"correo@ejemplo.com\"}";
 
         // Establecer el tipo de contenido JSON
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        RequestBody cuerpo = RequestBody.create(jsonDatos, JSON);
+        RequestBody cuerpo = RequestBody.create(jsonBody, JSON);
 
-        // Crear la solicitud POST
+        // Token de acceso
+        String token = "cb104ff5c93e2ae92c2d17acf48f6a4653c19844a20871d8f0f74e6a57616aa10089362f92de8b1cdc5e29ff136cbe0973ec37daa700dcec17c79d4acd82d6ccb4d6ce8cb1b4c89399af66246ea8a2fb2f97df6b3cce59efa211edf07df3f6978e1122be37c7f1d707217a577bc4e4a237af9c477044cf5be7cc6ead1233617a";
+
+        // Crear la solicitud POST con el encabezado de autorización
         Request solicitud = new Request.Builder()
             .url(url)
             .post(cuerpo)
+            .header("Authorization", "Bearer " + token)
             .build();
 
         try {
             Response respuesta = cliente.newCall(solicitud).execute();
 
             if (respuesta.isSuccessful()) {
-                System.out.println("Nueva entrada creada en Strapi con éxito.");
+                System.out.println("Nueva entrada creada exitosamente.");
             } else {
-                System.out.println("La solicitud a Strapi para crear una nueva entrada falló.");
+                System.out.println("La solicitud para crear una nueva entrada en Strapi ha fallado.");
+                System.out.println("Código de respuesta: " + respuesta.code());
+                System.out.println("Mensaje de error: " + respuesta.message());
+                System.out.println("Cuerpo de respuesta: " + respuesta.body().string());
             }
 
             // Cierra la respuesta
